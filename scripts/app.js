c42c5b95d235c671cb15a6e018070b68f951c680
Tyler Burgess <tyster23@gmail.com>
2015-11-06 15:18:08 -0700

    Calendar Tracker 2nd edition revised to look at
    Here it is?

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,23 @@
-var trackerApp = angular.module('trackerApp', ['ui.calendar', 'ui.bootstrap']);
-
+var trackerApp = angular.module('trackerApp', ['ui.calendar']);                 
 /**
  * calendarDemoApp - 0.9.0
  */
 
-trackerApp.controller(function CalendarCtrl($scope,$compile,uiCalendarConfig) {
+trackerApp.controller('CalendarCtrl', function CalendarCtrl($scope,$compile,uiCalendarConfig) {
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
     
     $scope.changeTo = 'Hungarian';
+    
     /* event source that pulls from google.com */
     $scope.eventSource = {
             url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
             className: 'gcal-event',           // an option!
             currentTimezone: 'America/Chicago' // an option!
     };
+    
     /* event source that contains custom events on the scope */
     $scope.events = [
       {title: 'All Day Event',start: new Date(y, m, 1)},
@@ -26,6 +27,7 @@ trackerApp.controller(function CalendarCtrl($scope,$compile,uiCalendarConfig) {
       {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
       {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
     ];
+    
     /* event source that calls a function on every view switch */
     $scope.eventsF = function (start, end, timezone, callback) {
       var s = new Date(start).getTime() / 1000;
@@ -44,18 +46,22 @@ trackerApp.controller(function CalendarCtrl($scope,$compile,uiCalendarConfig) {
           {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
         ]
     };
+    
     /* alert on eventClick */
     $scope.alertOnEventClick = function( date, jsEvent, view){
         $scope.alertMessage = (date.title + ' was clicked ');
     };
+    
     /* alert on Drop */
      $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
        $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
     };
+    
     /* alert on Resize */
     $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
        $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
     };
+    
     /* add and removes an event source of choice */
     $scope.addRemoveEventSource = function(sources,source) {
       var canAdd = 0;
@@ -69,6 +75,7 @@ trackerApp.controller(function CalendarCtrl($scope,$compile,uiCalendarConfig) {
         sources.push(source);
       }
     };
+    
     /* add custom event*/
     $scope.addEvent = function() {
       $scope.events.push({
@@ -78,26 +85,31 @@ trackerApp.controller(function CalendarCtrl($scope,$compile,uiCalendarConfig) {
         className: ['openSesame']
       });
     };
+    
     /* remove event */
     $scope.remove = function(index) {
       $scope.events.splice(index,1);
     };
+    
     /* Change View */
     $scope.changeView = function(view,calendar) {
       uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
     };
+    
     /* Change View */
     $scope.renderCalender = function(calendar) {
       if(uiCalendarConfig.calendars[calendar]){
         uiCalendarConfig.calendars[calendar].fullCalendar('render');
       }
     };
+     
      /* Render Tooltip */
     $scope.eventRender = function( event, element, view ) { 
         element.attr({'tooltip': event.title,
                      'tooltip-append-to-body': true});
         $compile(element)($scope);
     };
+    
     /* config object */
     $scope.uiConfig = {
       calendar:{
@@ -126,6 +138,7 @@ trackerApp.controller(function CalendarCtrl($scope,$compile,uiCalendarConfig) {
         $scope.changeTo = 'Hungarian';
       }
     };
+    
     /* event sources array*/
     $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
     $scope.eventSources2 = [$scope.calEventsExt, $scope.eventsF, $scope.events];
